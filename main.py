eb67b0ec841e59d58f30e96d643c31b93b5ea3a6
ᐯㄩ卄丨乙卂 ᐯㄩ卄 <47759698+dmitriy337@users.noreply.github.com>
2020-11-22 04:04:09 +0300

    Update main.py
    ClassHelloWorld

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print('HelloWorld')
+
+
+class PrintHelloW(object):
+	def PrintHelloWorldIntRaz(i):
+		for x in range(1,i):
+			print('HelloWorld')
+
+
+g=PrintHelloW 
+
+
+g.PrintHelloWorldIntRaz(10)
